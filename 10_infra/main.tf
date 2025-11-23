e8d7e29abf84032146e75a8088955f822bd9de02
taku <takumi.f.0308@icloud.com>
2025-11-24 00:00:45 +0900

    インフラの設定値修正

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -10,8 +10,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "{YOUR-S3-BUCKET-NAME}"
-    key     = "{YOUR-S3-BUCKET-KEY}"
+    bucket  = "cicd-lesson-terraform"
+    key     = "cicd-lesson-tf"
     region  = "ap-northeast-1"
     profile = "terraform"
   }
